3e30e7875e3f06d9038e20a6ec983bb0c3d77540
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2023-07-29 21:20:50 -0300

    Options Component

--- src/components/QuizBox.tsx
+++ src/components/QuizBox.tsx
@@ -2,20 +2,16 @@ import tw from "tailwind-styled-components";
 import TitleIllustration from "./TitleIllustration";
 
 const MainBox = tw.div`
+    relative
     flex
     flex-col
+    gap-8
     w-[29rem]
-    relative
     px-8
     py-[4.25rem]
     rounded-[1.5rem]
     bg-white
 `;
-
-const Title = tw.div`
-    absolute
-    -top-16
-    `;
 
 const Question = tw.div`
     text-[#2F527B]
@@ -26,12 +22,58 @@ const Question = tw.div`
 const Container = tw.div`
 relative`;
 
+const AnswerWrapper = tw.div`
+    flex
+    flex-col
+    gap-[1.56rem]        
+`;
+
+const Option = tw.div`
+    flex
+    gap-[2.80rem]
+    items-center
+    cursor-pointer
+    rounded-[0.75rem]
+    border-2
+    border-[#6066D0FF]
+    text-[#6066D0B4]
+    bg-white
+    hover:bg-[#F9A826]
+    hover:border-[#F9A826]
+    hover:text-white
+    w-full
+    h-[3.5rem]
+    px-[1.19rem]
+    transition-all
+
+    [&>div:nth-child(1)]:text-[1.5rem]
+    [&>div:nth-child(2)]:text-[1.125rem]
+`;
+
 export default function QuizBox() {
   return (
     <Container>
       <TitleIllustration />
       <MainBox>
         <Question>Kuala Lumpur is the capital of</Question>
+        <AnswerWrapper>
+          <Option>
+            <div>A</div>
+            <div>Vietnam</div>
+          </Option>
+          <Option>
+            <div>B</div>
+            <div>Vietnam</div>
+          </Option>
+          <Option>
+            <div>C</div>
+            <div>Vietnam</div>
+          </Option>
+          <Option>
+            <div>D</div>
+            <div>Vietnam</div>
+          </Option>
+        </AnswerWrapper>
       </MainBox>
     </Container>
   );
